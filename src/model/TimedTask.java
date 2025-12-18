295875244afe5bc43bab2638572a958b1211214f
alPHaa05 <alperhayir05@gmail.com>
2025-12-18 17:24:22 +0300

    TimeTask  sınıfına constructor eklendi

--- src/model/TimedTask.java
+++ src/model/TimedTask.java
@@ -1,8 +1,14 @@
 package model;
 
+import java.time.LocalDate;
+
 public class TimedTask extends Task {
 
     private Deadline deadline;
+
+    public TimedTask(Deadline deadline) {
+        this.deadline = deadline;
+    }
 
     public Deadline getDeadline() {
         return deadline;
