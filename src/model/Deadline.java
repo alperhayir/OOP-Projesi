98cfedd1f007f1427d151e8065d499faf7a59e56
alPHaa05 <alperhayir05@gmail.com>
2025-12-18 16:56:19 +0300

    Deadline sınıfına constructor eklendi

--- src/model/Deadline.java
+++ src/model/Deadline.java
@@ -5,6 +5,9 @@ import java.time.LocalDate;
 public class Deadline {
     private LocalDate dueDate;
 
+    public Deadline(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
     public LocalDate getDueDate() {
         return dueDate;
     }
