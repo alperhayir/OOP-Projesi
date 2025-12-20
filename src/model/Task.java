08622d612cea65a76dc948068bc1485b1e5e06c0
alPHaa05 <alperhayir05@gmail.com>
2025-12-20 16:19:48 +0300

    Task sınıfına constructor eklendi

--- src/model/Task.java
+++ src/model/Task.java
@@ -9,6 +9,14 @@ public class Task  implements Completable {
     private String description;
     private boolean completed;
 
+
+    public Task(String id,String title , String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
     @Override
     public void complete(){
 
