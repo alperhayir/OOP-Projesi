66232702bbc5cfbffcf6d399092d5e1a391f587b
alPHaa05 <alperhayir05@gmail.com>
2025-12-22 23:01:44 +0300

    User sınıfına görev listesi ve constructor eklendi

--- src/model/User.java
+++ src/model/User.java
@@ -1,7 +1,22 @@
 package model;
 
+import javax.management.relation.Role;
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private String id;
     private String name;
+
+    private List<Task> tasks;
+
+    public User(String id , String name ){
+        this.id = id;
+        this.name = name;
+        this.tasks = new ArrayList<>();
+    }
+
+
+
 }
