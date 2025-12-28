d0dedf79fd7db0154d2df0971962f722dab1dedd
alPHaa05 <alperhayir05@gmail.com>
2025-12-28 18:13:54 +0300

    Project sınıfı tamamlandı

--- src/model/Project.java
+++ src/model/Project.java
@@ -1,7 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Project {
 
     private String id;
     private String name;
+
+    private List<Task> tasks;
+
+
+    public Project(String id,String name) {
+        this.id = id;
+        this.name = name;
+        this.tasks = new ArrayList<>();
+    }
+
+    public List <Task> getTasks() {
+        return tasks;
+    }
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
 }
